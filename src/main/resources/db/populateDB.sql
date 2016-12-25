91b8c0b600dd3c78d6433558649a36beca6785b5
Anatolii Shirokov <tolikswc@yandex.ru>
2016-12-25 12:51:48 +0300

    add meals to initDB and populateDB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,3 +11,21 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('завтрак', 500, 100000);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('обед', 700, 100000);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('ужин', 800, 100000);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('завтрак', 500, 100001);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('обед', 700, 100001);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('ужин', 810, 100001);
