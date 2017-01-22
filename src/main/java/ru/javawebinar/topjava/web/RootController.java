44b7e787df570178df3a8e281ad1a419c5e1b4ed
Anatolii Shirokov <tolikswc@yandex.ru>
2017-01-22 16:24:15 +0300

    add meals to RootController add localization (bundle) to JSP: meal, meals

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -1,14 +1,27 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * User: gkislin
@@ -16,8 +29,13 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class RootController {
+    private static final Logger LOG = getLogger(RootController.class);
+
     @Autowired
-    private UserService service;
+    private UserService userService;
+
+    @Autowired
+    private MealRestController mealController;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
@@ -26,7 +44,7 @@ public class RootController {
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public String users(Model model) {
-        model.addAttribute("users", service.getAll());
+        model.addAttribute("users", userService.getAll());
         return "users";
     }
 
@@ -36,4 +54,62 @@ public class RootController {
         AuthorizedUser.setId(userId);
         return "redirect:meals";
     }
+
+    @RequestMapping(value = "/meals", method = RequestMethod.GET)
+    public String meals(Model model) {
+        model.addAttribute("meals", mealController.getAll());
+        return "meals";
+    }
+
+    @RequestMapping(value = "/meals", method = RequestMethod.POST)
+    public String meals(HttpServletRequest request, Model model) {
+        LocalDate startDate = DateTimeUtil.parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = DateTimeUtil.parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = DateTimeUtil.parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = DateTimeUtil.parseLocalTime(request.getParameter("endTime"));
+        model.addAttribute("meals", mealController.getBetween(startDate, startTime, endDate, endTime));
+        return "meals";
+    }
+
+    @RequestMapping(value = "/meal", method = RequestMethod.GET)
+    public String meal(HttpServletRequest request, Model model) {
+        final Meal meal;
+        if (request.getParameter("id") == null)
+            meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        else meal = mealController.get(getId(request));
+        model.addAttribute("meal", meal);
+        return "meal";
+    }
+
+    @RequestMapping(value = "/meal", method = RequestMethod.POST)
+    public String saveMeal(HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+
+        final Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.valueOf(request.getParameter("calories")));
+
+        if (request.getParameter("id").isEmpty()) {
+            LOG.info("Create {}", meal);
+            mealController.create(meal);
+        } else {
+            LOG.info("Update {}", meal);
+            mealController.update(meal, getId(request));
+        }
+        return "redirect:meals";
+    }
+
+    @RequestMapping(value = "/mealDelete", method = RequestMethod.GET)
+    public String mealDelete(HttpServletRequest request) {
+        mealController.delete(getId(request));
+        return "redirect:meals";
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.valueOf(paramId);
+    }
+
 }
+
--- src/main/webapp/WEB-INF/jsp/meal.jsp
+++ src/main/webapp/WEB-INF/jsp/meal.jsp
@@ -1,34 +1,36 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 
 <html>
-<head>
-    <title>Meal</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
+<jsp:include page="fragments/headTag.jsp"/>
 <body>
+<jsp:include page="fragments/bodyHeader.jsp"/>
+
 <section>
-    <h2><a href="index.html">Home</a></h2>
-    <h2>${param.action == 'create' ? 'Create meal' : 'Edit meal'}</h2>
+    <h2><c:if test="${meal.id==null}"><fmt:message key="meals.create"/></c:if>
+        <c:if test="${meal.id!=null}"><fmt:message key="meals.edit"/></c:if>
+    </h2>
     <hr>
     <jsp:useBean id="meal" type="ru.javawebinar.topjava.model.Meal" scope="request"/>
-    <form method="post" action="meals">
+    <form method="post" action="meal">
         <input type="hidden" name="id" value="${meal.id}">
         <dl>
-            <dt>DateTime:</dt>
+            <dt><fmt:message key="common.dateTime"/>:</dt>
             <dd><input type="datetime-local" value="${meal.dateTime}" name="dateTime"></dd>
         </dl>
         <dl>
-            <dt>Description:</dt>
+            <dt><fmt:message key="common.description"/>:</dt>
             <dd><input type="text" value="${meal.description}" size=40 name="description"></dd>
         </dl>
         <dl>
-            <dt>Calories:</dt>
+            <dt><fmt:message key="meals.calories"/>:</dt>
             <dd><input type="number" value="${meal.calories}" name="calories"></dd>
         </dl>
-        <button type="submit">Save</button>
-        <button onclick="window.history.back()">Cancel</button>
+        <button type="submit"><fmt:message key="common.save"/></button>
+        <button onclick="window.history.back()"><fmt:message key="common.cancel"/></button>
     </form>
 </section>
+<jsp:include page="fragments/footer.jsp"/>
 </body>
 </html>
