33124a78c34f0bd13e33ba31e5127abf403d9f35
Anatolii Shirokov <tolikswc@yandex.ru>
2017-01-22 14:10:09 +0300

    fix JDBC Tests

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,10 +1,15 @@
 package ru.javawebinar.topjava.service.jdbc;
 
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
+    @Override
+    public void testValidation() throws Exception {
+    }
 }
