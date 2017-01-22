5fce7fc8f1cb318720a9ffcc7f14a8ae83262c4f
Anatolii Shirokov <tolikswc@yandex.ru>
2017-01-22 16:39:59 +0300

    add ROLE_USER to Admin fix User Matcher: check roles

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.JpaUtil;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.ConstraintViolationException;
@@ -52,8 +51,8 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Test
     public void testGet() throws Exception {
-        User user = service.get(USER_ID);
-        MATCHER.assertEquals(USER, user);
+        User admin = service.get(ADMIN_ID);
+        MATCHER.assertEquals(ADMIN, admin);
     }
 
     @Test(expected = NotFoundException.class)
